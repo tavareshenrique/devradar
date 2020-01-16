75268ffe25c35457883851aa4c8f1ad1405dc3d9
Henrique Tavares <ihenrits@gmail.com>
2020-01-16 12:38:06 -0300

    adicionando cors no backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
+
+mongoose.set("useCreateIndex", true);
 
 mongoose.connect(
   "mongodb+srv://henrique:omnistack@cluster0-aue5v.mongodb.net/devradar?retryWrites=true&w=majority",
@@ -12,6 +15,7 @@ mongoose.connect(
   }
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
