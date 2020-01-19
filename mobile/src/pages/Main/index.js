bde39bae6d23f2b10061d9e7cc030db2dbbba134
Henrique Tavares <ihenrits@gmail.com>
2020-01-19 00:35:15 -0300

    finalizando app em react native do devradar

---
+++ mobile/src/pages/Main/index.js
@@ -0,0 +1,138 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync
+} from "expo-location";
+import { Marker, Callout } from "react-native-maps";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import api from "../../services/api";
+
+import {
+  MapContainer,
+  MarkedImage,
+  CalloutContainer,
+  Name,
+  Bio,
+  Techs,
+  SearchForm,
+  SearchInput,
+  LoadButton
+} from "./styles";
+
+export default function Main({ navigation }) {
+  const [devs, setDevs] = useState([]);
+  const [currentRegion, setCurrentRegion] = useState(null);
+  const [techs, setTechs] = useState("");
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        });
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude: -22.1055606,
+          longitude: -43.2125219,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04
+        });
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  async function loadDevs() {
+    const { latitude, longitude } = currentRegion;
+
+    const response = await api.get("/search", {
+      params: {
+        latitude,
+        longitude,
+        techs: techs
+      }
+    });
+
+    setDevs(response.data.devs);
+  }
+
+  function handleRegionChanged(region) {
+    setCurrentRegion(region);
+  }
+
+  if (!currentRegion) return null;
+
+  return (
+    <>
+      <MapContainer
+        onRegionChangeComplete={handleRegionChanged}
+        initialRegion={currentRegion}
+      >
+        {devs.map(dev => (
+          <Marker
+            key={dev._id}
+            coordinate={{
+              latitude: dev.location.coordinates[1],
+              longitude: dev.location.coordinates[0]
+            }}
+          >
+            <MarkedImage
+              source={{
+                uri: dev.avatar_url
+              }}
+            />
+            <Callout
+              onPress={() => {
+                navigation.navigate("Profile", {
+                  github_username: dev.github_username
+                });
+              }}
+            >
+              <CalloutContainer>
+                <Name>{dev.name}</Name>
+                <Bio>{dev.bio}</Bio>
+                <Techs>{dev.techs.join(", ")}</Techs>
+              </CalloutContainer>
+            </Callout>
+          </Marker>
+        ))}
+      </MapContainer>
+
+      <SearchForm>
+        <SearchInput
+          placeholder="Buscar devs por techs..."
+          style={styles.searchInput}
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+          value={techs}
+          onChangeText={setTechs}
+        />
+
+        <LoadButton onPress={loadDevs}>
+          <MaterialIcons name="my-location" size={20} color="#FFF" />
+        </LoadButton>
+      </SearchForm>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  searchInput: {
+    paddingHorizontal: 20,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 2
+  }
+});
---
+++ mobile/src/pages/Main/styles.js
@@ -0,0 +1,61 @@
+import styled from "styled-components/native";
+
+import MapView from "react-native-maps";
+
+export const MapContainer = styled(MapView)`
+  flex: 1;
+`;
+
+export const MarkedImage = styled.Image`
+  width: 54px;
+  height: 54px;
+  border-radius: 4px;
+  border-width: 4px;
+  border-color: #fff;
+`;
+
+export const CalloutContainer = styled.View`
+  width: 260px;
+`;
+
+export const Name = styled.Text`
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+export const Bio = styled.Text`
+  color: #666;
+  margin-top: 5px;
+`;
+
+export const Techs = styled.Text`
+  margin-top: 5px;
+`;
+
+export const SearchForm = styled.View`
+  position: absolute;
+  bottom: 20px;
+  left: 20px;
+  right: 20px;
+  z-index: 5;
+  flex-direction: row;
+`;
+
+export const SearchInput = styled.TextInput`
+  flex: 1;
+  height: 50px;
+  background-color: #fff;
+  color: #333;
+  border-radius: 25px;
+  font-size: 16px;
+`;
+
+export const LoadButton = styled.TouchableOpacity`
+  width: 50px;
+  height: 50px;
+  background-color: #8e4dff;
+  border-radius: 25px;
+  justify-content: center;
+  align-items: center;
+  margin-left: 15px;
+`;
